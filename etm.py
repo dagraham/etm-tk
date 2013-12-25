4336c217c12cc2d3fd00ccd580c7b00e38184ab4
Daniel Graham <daniel.graham@duke.edu>
2013-12-25 18:21:04 -0500

    Modal view and focus working - issue with adding BoxLayout twice.

--- etm.py
+++ etm.py
@@ -3,22 +3,15 @@ import sys
 import etmKv.etmData as etmData
 from etmKv.etmData import get_current_time, leadingzero
 
-# from datetime import date, datetime, timedelta
-# from dateutil.tz import tzlocal, gettz
-# from dateutil.parser import parse
-
 
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
-# from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.codeinput import CodeInput
 from pygments.lexers.special import TextLexer
 
 from kivy.uix.label import Label
-# from kivy.uix.popup import Popup
 from kivy.uix.modalview import ModalView
-# from kivy.uix.button import Button
 from kivy.properties import ObjectProperty
 from kivy.config import Config
 from kivy.clock import Clock
@@ -26,17 +19,51 @@ Config.set('graphics', 'height', '440')
 Config.set('graphics', 'width', '530')
 
 
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+from kivy.uix.codeinput import CodeInput
+from kivy.uix.modalview import ModalView
+
+
+class ETMDialog():
+
+    def __init__(self, parent=None, prompt="etm", validator=lambda x: True):
+        self.parent = parent
+        self.prompt = prompt
+        self.validator = validator
+        self.text = ''
+
+        self.content = BoxLayout(orientation='vertical')
+        self.content.add_widget(Label(text=self.prompt))
+        self.input = CodeInput(multiline=False, size_hint=(1, None), height=30, lexer=TextLexer())
+        self.input.bind(on_text_validate=self.validate)
+        # self.input.bind(on_dismiss=self.dismiss)
+        self.content.add_widget(self.input)
+
+    def run(self):
+        self.popup = ModalView(size_hint=(None, None), size=(400, 200))
+        self.popup.add_widget(self.content)
+        # self.parent.input_wid.focus = False
+        self.input.focus = True
+        self.popup.open()
+
+    def validate(self, value):
+        print('validate', value, self.input.text)
+        if self.validator(self.input.text):
+            self.parent.output_wid.text = self.input.text
+            self.input.focus = False
+            self.popup.dismiss()
+            self.parent.input_wid.focus = True
+
+
 class ETMTextInput(TextInput):
 
     history = []
     index = 0
     now = None
-    status_wid = ObjectProperty()
-    input_wid = ObjectProperty()
-    output_wid = ObjectProperty()
     options = {}
     popup = ''
-
+    value = ''
 
     def _keyboard_on_key_down(self, window, keycode, text, modifiers):
         # print(keycode)
@@ -56,28 +83,20 @@ class ETMTextInput(TextInput):
                 window, keycode, text, modifiers)
 
     def init(self):
+        self.Dialog = ETMDialog(parent=self, prompt="etm text")
         self.loop = loop
         res = self.loop.do_command('s')
         self.options = self.loop.options
-        # print(self.options)
         self.start_timer()
         return(res)
 
     def start_timer(self):
         self.now = get_current_time()
         nxt = (60 - self.now.second)
-        if self.status_wid:
-            nowfmt = "{0} {1}".format(
-                self.now.strftime(self.options['reprtimefmt']).lower(),
-                self.now.strftime("%a %b %d %Z"))
-            nowfmt = leadingzero.sub("", nowfmt)
-            self.status_wid.text = nowfmt
-        else:
-            nxt = 1
+        print(self.now)
         Clock.schedule_once(self.timer_callback, nxt)
 
     def timer_callback(self, dt):
-        # print('In callback')
         self.start_timer()
 
     def process_input(self):
@@ -96,63 +115,18 @@ class ETMTextInput(TextInput):
             try:
                 res = loop.do_command(cmd)
             except:
-                res = self.my_dialog()
-                # res = self.dialog
-                print('got here')
+                self.Dialog.run()
+                res = self.Dialog.text
+                print('res', res)
+                self.output_wid.text = self.text
+                self.output_wid.scroll_y = 1
+                self.output_wid.readonly = False
+                self.output_wid.cursor = (0, 0)
+
             if not res:
                 return(True)
             self.output_wid.text = res
             self.output_wid.scroll_y = 1
-
-    # def show_popup(self):
-    #     y = max(self.output_wid.minimum_height, self.output_wid.height)
-    #     print('y', y)
-    #     btnclose = Button(text='Save', size_hint_y=None, height='40sp')
-    #     btnsave = Button(text='Cancel', size_hint_y=None, height='40sp')
-    #     buttons = BoxLayout(orientation='horizontal', height='40sp')
-    #     buttons.add_widget(btnclose)
-    #     buttons.add_widget(btnsave)
-    #     content = BoxLayout(orientation='vertical')
-    #     # content.add_widget(Label(text='Hello world'))
-    #     content.add_widget(CodeInput(multiline=True, size_hint=(1, None), height=350, focus=True, lexer=TextLexer()))
-    #     content.add_widget(buttons)
-    #     popup = ModalView(size_hint=(None, None), size=(400, 400))
-    #     popup.add_widget(content)
-    #     btnclose.bind(on_release=popup.dismiss)
-    #     btnsave.bind(on_release=popup.dismiss)
-    #     popup.open()
-
-    def my_dialog(self):
-        def validate(value):
-            print('validate', value)
-            self.output_wid.text = input.text
-            self.output_wid.scroll_y = 1
-            self.output_wid.readonly = False
-            self.output_wid.focus = True
-            self.output_wid.cursor = (0, 0)
-            popup.dismiss()
-
-        content = BoxLayout(orientation='vertical')
-        content.add_widget(Label(text='etm'))
-        input = CodeInput(multiline=False, size_hint=(1, None), height=30, focus=True, lexer=TextLexer())
-        content.add_widget(input)
-        popup = ModalView(size_hint=(None, None), size=(400, 200))
-        popup.add_widget(content)
-        input.bind(on_text_validate=validate)
-        popup.open()
-
-    # def on_enter(self, instance):
-    #     print('User pressed enter in', instance)
-
-    # def on_text(self, instance, value):
-    #     self.popup = value
-    #     print("value", self.popup)
-
-    # def validate(self, instance):
-    #     print("final value", self.popup)
-    #     self.output_wid.text = self.popup
-    #     self.output_wid.scroll_y = 1
-    #     return(True)
 
     def previous_history(self):
         """
@@ -172,11 +146,9 @@ class ETMTextInput(TextInput):
 
 
 class BoxIOWidget(BoxLayout):
-    def set_text(self, *args):
-        fo = open('./data.txt', 'r')
-        s = "\n".join(fo.readlines())
-        fo.close()
-        return unicode(s, 'utf-8')
+    input_wid = ObjectProperty()
+    status_wid = ObjectProperty()
+    output_wid = ObjectProperty()
 
 
 class etmApp(App):
