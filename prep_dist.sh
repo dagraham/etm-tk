1ec49a45a145bd367d1d76cdf5410c4d5ab3fc97
Dan Graham <dnlgrhm@gmail.com>
2014-05-21 13:18:29 -0400

    Only use lightweight tag when not incrementing version number.

--- prep_dist.sh
+++ prep_dist.sh
@@ -75,6 +75,12 @@ Edit etmTk/v.py to change the major and minor numbers."
 #    hg tag $tag -f
 #    git tag -a $tag -m "$versioninfo" HEAD
     echo "Updated to $tag [$versioninfo]" >> $logfile
+    echo "version = \"$tag\"" > /Users/dag/etm-tk/etmTk/v.py
+    echo "version = \"$tag [$versioninfo]\"" > etmTk/version.py
+    echo "$tag [$versioninfo]" > version.txt
+    git add etmTk/v.py etmTk/version.py
+    git commit -a -m "tagged version $tag"
+    git tag -a -f $tag -m "$versioninfo" HEAD
 else
     # drop the abbrev header and a trailing -0, if there is one
 #    tag=`git describe --tags --long | sed 's/-[^\-]*$//g' | sed 's/-0$//g'`  # something like 0.0.65-2
@@ -82,13 +88,8 @@ else
     change="retaining version $vinfo."
     echo "Using $tag [$versioninfo]" >> $logfile
     # hg tag $tag -f
+    git tag -f $tag HEAD
 fi
-echo "version = \"$tag\"" > /Users/dag/etm-tk/etmTk/v.py
-echo "version = \"$tag [$versioninfo]\"" > etmTk/version.py
-echo "$tag [$versioninfo]" > version.txt
-git add etmTk/v.py etmTk/version.py
-git commit -a -m "tagged version $tag"
-git tag -a -f $tag -m "$versioninfo" HEAD
 
 echo $tag > etmTk/v.txt
 
